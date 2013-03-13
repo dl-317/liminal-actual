9ec8e9c7158a245ba723831bde4651da2e7e1263
DL-317 <dl-317@escape27.net>
2013-03-12 19:32:22 -0700

    brought additional interfaces online

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express")
 var app = express();
 
-app.get("*", function(req, res) {
+app.use(function(req, res, next) {
     var body = "system online";
     res.writeHead(404, {
         "Content-Type": "text/plain",
