5b64550c26c6dc7b3418675c921a0977b4a813b8
DL-317 <dl-317@escape27.net>
2013-03-12 20:22:32 -0700

    enabled system failures

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express")
 var app = express();
 
+// static content goes in static
 app.use(express.static("static"));
 
+// fallback to simple system online message
 app.use(function(req, res, next) {
     var body = "system online";
     res.writeHead(404, {
@@ -12,5 +14,16 @@ app.use(function(req, res, next) {
     res.end(body);
 });
 
+// log errors and present system failure message
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    var body = "system failure";
+    res.writeHead(500, {
+        "Content-Type": "text/plain",
+        "Content-Length": body.length
+    });
+});
+
+// begin listening for requests
 app.listen("2727");
 console.log("Listening on port 2727");
